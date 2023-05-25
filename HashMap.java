65f141e34bf8fbeb44645be6afacded74c0318cd
AravaBhargavi <134575954+AravaBhargavi@users.noreply.github.com>
2023-05-25 12:06:49 +0530

    Add files via upload

---
+++ HashMap.java
@@ -0,0 +1,11 @@
+public class HashMap<T, T1> {
+    public void put(T1 bro, T1 pizza) {
+    }
+
+    public boolean containsKey(T1 userID) {
+        return false;
+    }
+    public Object get(T1 userID) {
+        return null;
+    }
+}
